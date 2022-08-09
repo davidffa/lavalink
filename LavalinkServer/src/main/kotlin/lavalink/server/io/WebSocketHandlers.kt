1f8a322b9a56eb27703f05df878423a03306b4ca
David Amorim <davidfilipeamorim@gmail.com>
2022-08-09 15:39:58 +0100

    Remove unused import

--- LavalinkServer/src/main/kotlin/lavalink/server/io/WebSocketHandlers.kt
+++ LavalinkServer/src/main/kotlin/lavalink/server/io/WebSocketHandlers.kt
@@ -34,7 +34,6 @@ import moe.kyokobot.koe.VoiceServerInfo
 import org.json.JSONObject
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.util.*
 import kotlin.collections.HashSet
 
 class WebSocketHandlers {
