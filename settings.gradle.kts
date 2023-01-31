55a78c893fbde58b843b18573b2811df9b28d222
David Amorim <davidfilipeamorim@gmail.com>
2023-01-31 01:32:10 +0000

    Update lavaplayer

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -32,7 +32,7 @@ dependencyResolutionManagement {
       library("netty-epoll", "io.netty", "netty-transport-native-epoll").version("4.1.87.Final")
       library("netty-kqueue", "io.netty", "netty-transport-native-kqueue").version("4.1.87.Final")
 
-      library("lavaplayer-main", "com.github.davidffa", "lavaplayer-fork").version("6331ea9")
+      library("lavaplayer-main", "com.github.davidffa", "lavaplayer-fork").version("26f27d0")
       library("lavaplayer-iprotator", "com.sedmelluq", "lavaplayer-ext-youtube-rotator").version("0.2.3")
 
       library("lavadsp", "com.github.davidffa", "lavadsp-fork").version("0.7.9")
