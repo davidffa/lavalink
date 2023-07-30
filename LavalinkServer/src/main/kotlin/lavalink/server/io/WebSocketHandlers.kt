447cc4e48b2da6a99d75d543ab0d58d392ea326a
David Amorim <davidfilipeamorim@gmail.com>
2023-07-30 11:40:48 +0100

    Remove deprecation warning

--- LavalinkServer/src/main/kotlin/lavalink/server/io/WebSocketHandlers.kt
+++ LavalinkServer/src/main/kotlin/lavalink/server/io/WebSocketHandlers.kt
@@ -90,10 +90,6 @@ class WebSocketHandlers {
 
     player.setPause(json.optBoolean("pause", false))
     if (json.has("volume")) {
-      if (!loggedVolumeDeprecationWarning) log.warn("The volume property in the play operation has been deprecated" +
-        "and will be removed in v4. Please configure a filter instead. Note that the new filter takes a " +
-        "float value with 1.0 being 100%")
-      loggedVolumeDeprecationWarning = true
       val filters = player.filters ?: FilterChain()
       filters.volume = json.getFloat("volume") / 100
       player.filters = filters
